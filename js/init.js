24382bd99592c43a0e9e5f8435ace18676b7a5f2
Milton Lima <milton.lima@outlook.com>
2020-01-22 22:05:55 -0300

    Criado acesso ao Firebase e scrips em JS

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD80cN9H9ZtRaEUw4cHtrzFXoM8ooTsAHk",
+    authDomain: "linkme-7232b.firebaseapp.com",
+    databaseURL: "https://linkme-7232b.firebaseio.com",
+    projectId: "linkme-7232b",
+    storageBucket: "linkme-7232b.appspot.com",
+    messagingSenderId: "387530389471",
+    appId: "1:387530389471:web:7ff5dfc348a79d15fa766a"
+};
+firebase.initializeApp(firebaseConfig);
---
+++ js/linkme.js
@@ -0,0 +1,11 @@
+function enviar(){
+    var emailValue = document.getElementById('emailTxt').value;
+    var areaValue = document.getElementById('areaTxt').value;
+    var timestamp = new Date().getTime();
+    // A variavel database irá receber as funcções de acesso ao banco de dados
+    var database = firebase.database();
+        database.ref(timestamp).set({
+            email: emailValue,
+            messagem: areaValue
+    }); 
+}
